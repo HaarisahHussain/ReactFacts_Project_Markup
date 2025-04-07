d5d5492e7263f88be3c080cd1cd3cd5cd4d12657
hhussain <haarisah73@gmail.com>
2025-04-07 15:45:30 +0100

    Project Finished

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,26 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+const root = createRoot(document.getElementById('root'))
+
+root.render(
+  <main>
+      <img src="React_(web_framework)-Logo.wine.png" width="40px" alt="React logo"/>
+      <h1>Fun Facts about React!</h1>
+      <ul>
+        <li>Was First Released in 2013</li>
+        <li>Was orignally created by Jordan Walker</li>
+        <li>Has well over 200k stars on GitHub</li>
+        <li>Is Maintained by Meta</li>
+        <li>Powers thousands of enterprise apps, including mobile appls</li>
+      </ul>
+      
+  </main>
+
+
 )
+ 
+
+
+ 
+
